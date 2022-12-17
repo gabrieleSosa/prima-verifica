a88ccb9c07709cec28b53c410fede3cfcd93309a
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2022-12-17 08:29:25 +0100

    .

---
+++ static/libs/js/index1.js
@@ -0,0 +1,7 @@
+let latoInput = document.querySelector("[name=lato]")
+
+latoInput.addEventListener("input", function (e) {
+    
+    lato = parseInt(latoInput.value);
+    console.log(`lato cambiato a --> ${lato}`)
+})
