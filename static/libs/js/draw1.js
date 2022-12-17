bf5ef566cc9f62455e92e0588b73d4864fe66f21
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2022-12-17 09:07:49 +0100

    primo esercizio finito

--- static/libs/js/draw1.js
+++ static/libs/js/draw1.js
@@ -15,28 +15,29 @@ function pulizia() {
 function setup() {
     createCanvas(w,h)
     background("black")
+    stroke("white")
 }
 
 function draw() {
     
     let r = random()
-    square(x, y, lato)
-    x=x+lato
+    
+    // square(x, y, lato)
+    
 
+    if (r>0.5) {
+        
+        //line(30, 20, 85, 20);
+        line(x+lato, y, x, y+lato)
+        
+    } else {
+        line(x, y, x+lato, y+lato)
+    }
+
+    x=x+lato
     if (x>=400) {
         
         x=0
         y=y+lato
     }
-
-    if (r>0.5) {
-        
-        line(30, 20, 85, 20);
-        line(x+lato, y, x, )
-        stroke("black")
-    } else {
-        
-    }
-
-    line()
 }
---
+++ static/libs/js/draw2.js
@@ -0,0 +1,15 @@
+lato = 40
+
+let x = 0
+let y = 0
+
+function setup() {
+    createCanvas(400,400)
+    background("black")
+}
+
+function draw() {
+    
+    let r = random()
+    
+}
